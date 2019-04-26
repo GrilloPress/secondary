9d22c3959ecb280fb6f364bc265926c17c829e89
grillopress <grillopress@gmail.com>
2019-04-26 15:56:25 +0100

    add eligibility

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,80 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+
+
+
+router.post('/register-as-a-data-donor/v1/eligibility/where-do-you-live', function (req, res) {
+  let answer = req.body.live;
+
+  if (answer === 'england') { // the || means this OR that
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/is-your-GP-in-scotland-or-wales')
+
+  } else if (answer === 'northernIreland') {
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/outcome/3')
+
+  } else if (answer === 'scotland' || 'wales' ) {
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/is-your-GP-in-england')
+
+  } else {
+    res.redirect('error')
+  }
+});
+
+router.post('/register-as-a-data-donor/v1/eligibility/is-your-GP-in-england', function (req, res) {
+  let answer = req.body.gpBorderEngland;
+
+  if (answer === 'yes') { // the || means this OR that
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/outcome/2')
+
+  } else if (answer === 'no' || 'unsure' ) {
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/outcome/3')
+
+  } else {
+    res.redirect('error')
+  }
+});
+
+router.post('/register-as-a-data-donor/v1/eligibility/is-your-GP-in-scotland-or-wales', function (req, res) {
+  let answer = req.body.gpBorderScotlandWales;
+
+  if (answer === 'yes') { // the || means this OR that
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/outcome/2')
+
+  } else if (answer === 'no' || 'unsure' ) {
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/outcome/1')
+
+  } else {
+    res.redirect('error')
+  }
+});
+
+router.post('/register-as-a-data-donor/v1/eligibility/are-you-13-or-above', function (req, res) {
+  let answer = req.body.age;
+
+  if (answer === 'yes') { // the || means this OR that
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/where-do-you-live')
+
+  } else if (answer === 'no' ) {
+
+    res.redirect('/register-as-a-data-donor/v1/eligibility/outcome/age')
+
+  } else {
+    res.redirect('error')
+  }
+});
+
+
 // Add your routes here - above the module.exports line
 
 module.exports = router;
