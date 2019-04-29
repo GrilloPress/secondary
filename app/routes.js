75cff18bc5643c344c3ecdb94fb217c8ecadd426
Samantha Saw <samantha.saw@nhs.net>
2019-04-29 12:01:32 +0100

    Updated the javascript for email and text and updated the v1 index file

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,20 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post('/register-as-a-data-donor/v1/register/weve-added-you', function (req, res) {
+  let answer = req.body.confirmationChannel;
+
+  if (answer === 'email') { // the || means this OR that
+
+    res.redirect('/register-as-a-data-donor/v1/contact-preference/future-contact-preference-email')
+
+  } else if (answer === 'text' ) {
+
+    res.redirect('/register-as-a-data-donor/v1/contact-preference/future-contact-preference-text')
+
+  } else {
+    res.redirect('error')
+  }
+});
+
 module.exports = router;
